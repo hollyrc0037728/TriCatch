03c8fa2397d8a83c72dc6d37604f4306ffa42df3
Holly Carr <rc0037728@student.riversidecollege.ac.uk>
2019-11-25 15:42:22 +0000

    addedtry ctach

--- TriCatch/Form1.cs
+++ TriCatch/Form1.cs
@@ -15,6 +15,38 @@ namespace TriCatch
         public Form1()
         {
             InitializeComponent();
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
+            richTextBox1.AppendText(i.ToString() + "\n");
+            richTextBox1.AppendText(d.ToString() + "\n");
+
+            string st = "Three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an interger");
+            }
+
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + " is not an integer");
+                int i2 = 0;
+            }
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
